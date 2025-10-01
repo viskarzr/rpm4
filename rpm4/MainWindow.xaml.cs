6a1b94db6ed347f8244a9dd0582a0dddef20cbff
Кирюшова В <-31@LAB17-05>
2025-10-01 15:27:03 +0300

    визуал

--- rpm4/MainWindow.xaml.cs
+++ rpm4/MainWindow.xaml.cs
@@ -20,5 +20,17 @@ namespace rpm4
         {
             InitializeComponent();
         }
+
+ 
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №4\r\nРабота с классами. Перегрузка методов.Создать класс Triangle (треугольник) с полями-сторонами.\r\n Создать необходимые методы и свойства. Определить метод вычисления периметра.\r\n Создать перегруженные методы SetParams, для установки параметров объекта,\r\n в том числе увеличения размеров треугольника в 2 раза.\r\nВыполнила:\r\nСтудентка гр.ИСП-31\r\nКирюшова Виктория");
+        }
     }
 }
